7976965e4a26aaded24e37fa2dbc39c53c3b2e4c
bowenliang123 <liang.bowen.123@qq.com>
2026-08-12 15:13:00 +0800

    refactor(text_utils): remove redundant contains_japanese and simplify normalize_line_breaks
    - contains_japanese duplicated CHINESE_CHAR_PATTERN for kanji and
      JAPANESE_KANA_PATTERN for kana; remove it and update callers to use the
      two precise checks.
    - Simplify normalize_line_breaks by dropping the unnecessary pre-check.
    - Remove the corresponding export from md_exporter.utils.__init__.

--- md_exporter/utils/__init__.py
+++ md_exporter/utils/__init__.py
@@ -4,7 +4,7 @@ from .markdown_utils import convert_markdown_to_html, get_md_text, strip_markdow
 from .mimetype_utils import MimeType
 from .param_utils import get_md_text_from_tool_params, get_param_value
 from .table_utils import SUGGESTED_SHEET_NAME, extract_headings, parse_md_to_tables
-from .text_utils import contains_chinese, contains_japanese, normalize_line_breaks, remove_think_tags
+from .text_utils import contains_chinese, normalize_line_breaks, remove_think_tags
 
 __all__ = [
     # file_utils
@@ -26,7 +26,6 @@ __all__ = [
     "SUGGESTED_SHEET_NAME",
     # text_utils
     "contains_chinese",
-    "contains_japanese",
     "remove_think_tags",
     "normalize_line_breaks",
 ]
